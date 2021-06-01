4616a5160d57c410808947de44596e6dad45c717
RachelChen <iceage91@gmail.com>
2021-06-01 18:30:03 +0800

    feat: add function to keep user's option

--- app.js
+++ app.js
@@ -14,15 +14,26 @@ app.get('/', (req, res) => {
   if (!app.locals.partials) {
     app.locals.partials = {}
   }
-  app.locals.partials.targets = targets.results
-  res.render('index')
+  app.locals.partials.targets = []
+  app.locals.partials.targets.push(...targets.results)
+  return res.render('index')
 })
 app.post('/', (req, res) => {
-  const targetID = req.body.target
+  const id = req.body.id
   // Generate a trash talk for a target based on it's id
-  const trashTalk = generateTrashTalk(targetID)
-  res.render('index', { trashTalk })
+  const trashTalk = generateTrashTalk(id)
+  // this will reserve the option that was chosed by user last turn
+  const targetList = app.locals.partials.targets.map(target => {
+    if (target.id.toString() === id) {
+      target.checked = true
+    } else {
+      target.checked = false
+    }
+  })
+  return res.render('index', { trashTalk })
 })
+
+
 
 app.listen(port, () => {
   console.log(`The server running on http://localhost:${port}`)
